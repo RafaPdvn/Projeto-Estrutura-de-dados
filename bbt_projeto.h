945ef70cdc11f2a87f2714df84fb5226281dbdba
Farael <faelpadovani03@hotmail.com>
2022-03-20 20:15:51 -0300

    update

--- bbt_projeto.h
+++ bbt_projeto.h
@@ -7,6 +7,8 @@ typedef struct sOpc{
     int vez;
 }Opc;
 
+
+//Funções Fila
 typedef struct sNof{
     Opc info;
     struct sNof *prox;
@@ -64,19 +66,68 @@ void desenfileirar(Fila *ptr){
         if(ptr->fim == ptr->inicio){
             ptr->fim = NULL;
             ptr->inicio = NULL;
-            printf("Elemento desalocado com sucesso\n\n");
         }else{
             ptr->inicio = aux->prox;
             des(aux);
-            printf("Elemento desalocado com sucesso\n\n");
         }
+    }
+}
+
+
+
+//Funções Pilha
+typedef struct sNOp{
+    Opc info;
+    struct sNOp *prox;
+}NOp;
+
+void iniciar(NOp** topo){
+    topo = NULL;
+}
+
+NOp* alocarNo(){
+    return(NOp*)malloc(sizeof(NOp));
+}
+
+void desalocarNo(NOp *topo){
+    free(topo);
+}
+
+int vazia(NOp *topo)
+{
+    if(topo == NULL)
+        return 1;
+    return 0;
+}
+
+void empilhar(NOp** topo, Opc elem){
+    NOp *novo;
+    novo = alocarNo();
+    if(novo != NULL){
+        novo -> info = elem;
+        novo -> prox = *topo;
+        *topo = novo;
+
     }else{
-        printf("Erro: fila vazia\n\n");
+        printf("Problema na alocacao!\n");
     }
 
 }
 
+void desempilhar(NOp** topo){
+    if(!vazia(topo)){
+        NOp* aux;
+        aux = *topo;
+        *topo = aux -> prox;
+        desalocarNo(aux);
+    }else{
+        printf("Pilha vazia!\n");
+    }
+}
 
+
+
+//Funções de impressão
 void imprime_tab(char tab[8][8]){
     printf("|---|---|---|---|---|---|---|---|\n");
     for(int i = 0; i < 8; i++){
@@ -109,16 +160,19 @@ void imprime_comando(int fase){
                 printf("1) |F| |F| \n");
                 printf("2) |F| |E| |F|\n");
                 printf("3) |D| |F| |F|\n");
+                printf("4) |F| |D| \n");
                 break;
             case 2:
-                printf("1) |F| |F| \n");
-                printf("2) |F| |E| |F|\n");
-                printf("3) |D| |F| |F|\n");
+                printf("1) |D| |F| |F| |E|\n");
+                printf("2) |D| |F| |E| |F|\n");
+                printf("3) |F| |F| |F|\n");
+                printf("4) |E| |D| |E|\n");
                 break;
             case 3:
                 printf("1) |F| |F| \n");
-                printf("2) |F| |E| |F|\n");
-                printf("3) |D| |F| |F|\n");
+                printf("2) |D| |F| |F| |F|\n");
+                printf("3) |E| |F| \n");
+                printf("4) |E| |E| |F| |F|\n");
                 break;
 
             default:
@@ -127,6 +181,8 @@ void imprime_comando(int fase){
 }
 
 
+
+//Funções de comandos de movimentação
 void F(char tab[8][8], char plr[3][3]){
     int c, l;
 
@@ -143,29 +199,21 @@ void F(char tab[8][8], char plr[3][3]){
         if((c+1 < 8)&&(tab[l][c+1]!='X')){
             tab[l][c] = ' ';
             tab[l][c+1] = 'P';
-        }else{
-            printf("Impossivel mover nesta dire��o!\n\n");
         }
     }else if(plr[0][1] != ' '){ //Movimenta para Cima
         if((l-1 > 0)&&(tab[l-1][c]!='X')){
             tab[l][c] = ' ';
             tab[l-1][c] = 'P';
-        }else{
-            printf("Impossivel mover nesta dire��o!\n\n");
         }
     }else if(plr[1][0] != ' '){ //Movimenta a Esquerda
         if((c-1 > 0)&&(tab[l][c-1]!='X')){
             tab[l][c] = ' ';
             tab[l][c-1] = 'P';
-        }else{
-            printf("Impossivel mover nesta dire��o!\n\n");
         }
     }else if(plr[2][1] != ' '){ //Movimenta para Baixo
         if((l+1 < 8)&&(tab[l+1][c]!='X')){
             tab[l][c] = ' ';
             tab[l+1][c]= 'P';
-        }else{
-            printf("Impossivel mover nesta dire��o!\n\n");
         }
     }
 }
@@ -202,128 +250,79 @@ void D(char plr[3][3]){
     }
 }
 
-
-void executar_comandos(Fila *fila, int fase, char tab[8][8], char plr[3][3]){
-    switch (fase){
-        //Comandos fase 1
+void executar_comando1(Fila *fila, int fase, char tab[8][8], char plr[3][3]){
+    switch ((fila->inicio)->info.cmd){
         case 1:
-            switch ((fila->inicio)->info.cmd){
-                case 1:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){
-                        F(tab,plr);
-                        F(tab,plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-                case 2:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){
-                        F(tab,plr);
-                        E(plr);
-                        F(tab,plr);   
-                    }
-                    desenfileirar(fila);
-                    break;
-                case 3:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){    
-                        D(plr);
-                        F(tab,plr);
-                        F(tab,plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-                case 4:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){      
-                        F(tab,plr);
-                        D(plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-                default:
-                    break;
+            for(int i = (fila->inicio)->info.vez ; i > 0; i--){
+                F(tab,plr);
+                F(tab,plr);
             }
+            desenfileirar(fila);
             break;
-        //Comandos fase 2
         case 2:
-            switch ((fila->inicio)->info.cmd){
-                case 1:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){      
-                        D(plr);
-                        F(tab,plr);
-                        F(tab,plr);
-                        E(plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-                case 2:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){      
-                        D(plr);
-                        F(tab,plr);
-                        E(plr);
-                        F(tab,plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-                case 3:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){      
-                        F(tab,plr);
-                        F(tab,plr);
-                        F(tab,plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-
-                default:
-                    break;
+            for(int i = (fila->inicio)->info.vez ; i > 0; i--){
+                F(tab,plr);
+                E(plr);
+                F(tab,plr);
             }
+            desenfileirar(fila);
             break;
-        //Comandos fase 3
         case 3:
-            switch ((fila->inicio)->info.cmd){
-                case 1:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){      
-                        F(tab,plr);
-                        F(tab,plr);
-                        F(tab,plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-                case 2:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){      
-                        D(plr);
-                        F(tab,plr);
-                        F(tab,plr);
-                        F(tab,plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-                case 3:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){      
-                        F(tab,plr);
-                        E(plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-                case 4:
-                    for(int i = (fila->inicio)->info.vez ; i > 0; i--){      
-                        E(plr);
-                        E(plr);
-                        F(tab,plr);
-                        F(tab,plr);
-                    }
-                    desenfileirar(fila);
-                    break;
-
-                default:
-                    break;
+            for(int i = (fila->inicio)->info.vez ; i > 0; i--){
+                D(plr);
+                F(tab,plr);
+                F(tab,plr);
             }
+            desenfileirar(fila);
             break;
-
-            default:
-                break;
+        case 4:
+            for(int i = (fila->inicio)->info.vez ; i > 0; i--){
+                F(tab,plr);
+                D(plr);
+            }
+            desenfileirar(fila);
+            break;
+        default:
+            break;
     }
 }
 
-
+void executar_comando2(NOp *pilha, int fase, char tab[8][8], char plr[3][3]){
+    switch (pilha->info.cmd){
+        case 1:
+            for(int i = pilha->info.vez ; i > 0; i--){
+                F(tab,plr);
+                F(tab,plr);
+            }
+            desenfileirar(pilha);
+            break;
+        case 2:
+            for(int i = pilha->info.vez ; i > 0; i--){
+                F(tab,plr);
+                E(plr);
+                F(tab,plr);
+            }
+            desenfileirar(pilha);
+            break;
+        case 3:
+            for(int i = pilha->info.vez ; i > 0; i--){
+                D(plr);
+                F(tab,plr);
+                F(tab,plr);
+            }
+            desenfileirar(pilha);
+            break;
+        case 4:
+            for(int i = pilha->info.vez ; i > 0; i--){
+                F(tab,plr);
+                D(plr);
+            }
+            desenfileirar(pilha);
+            break;
+        default:
+            break;
+    }
+}
 
 
 
--- projeto.c
+++ projeto.c
@@ -5,20 +5,23 @@
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-    int fase = 1, ctrl;
+    int fase = 1, ctrl, tente = 0;
     Opc elem;
     Fila *fila;
     fila = (Fila*)malloc(sizeof(Fila));
     inic(fila);
+    NOp *pilha;
+    pilha = (NOp*)malloc(sizeof(NOp));
+    iniciar(pilha);
 
-    char tab[8][8] = {'P','X',' ',' ',' ','X',' ',' ',
-                      ' ','X',' ',' ',' ','X',' ',' ',
+    char tab[8][8] = {'P',' ','X',' ',' ','X',' ',' ',
+                      ' ',' ','X',' ',' ','X',' ',' ',
                       ' ',' ',' ',' ',' ','X',' ',' ',
                       ' ',' ',' ',' ',' ',' ',' ',' ',
                       'X','X','X',' ','X',' ',' ',' ',
                       ' ',' ',' ',' ','X',' ','X','X',
                       ' ',' ',' ',' ',' ',' ',' ',' ',
-                      ' ',' ',' ','X','X','X',' ','O',};
+                      ' ',' ','X','X','X',' ',' ','O',};
 
     char plr[3][3] = {' ',' ',' ',
                       ' ','P','>',
@@ -34,30 +37,78 @@ int main(){
         printf("\nInsira a sequencia de comando para o Player 'P' chegar ao objetivo 'O'\n");
         imprime_comando(fase);
         //Solicitar comando do usuario
-        do{
-             printf("\nDigite o comando: ");
-             scanf("%d", &elem.cmd);
-             printf("Digite o numero de vezes que este comando serao executado: ");
-             scanf("%d", &elem.vez);
-             enfileirar(fila, elem);
-             printf("\nDeseja inserir outro comando? 1 p/ sim, 0 p/ nao: ");
-             scanf("%d", &ctrl);
-         }while(ctrl!=0);
-        //Mover P
-        do{
-            printf("\n|------------------------------|\n\n");
-            executar_comandos(fila, fase, tab, plr);
-            imprime_tab(tab);
-            imprime_player(plr);
-            if(tab[7][7] == 'P'){
-                printf("Parabens!!! Voc� concluiu a %d� fase\n\n",fase);
-                fase++;
-                tab[7][7] = 'O';
-                tab[0][0] = 'P';
+        switch (fase){
+            case 1:
+                do{
+                    printf("\nDigite o comando: ");
+                    scanf("%d", &elem.cmd);
+                    printf("Digite o numero de vezes que este comando serao executado: ");
+                    scanf("%d", &elem.vez);
+                    enfileirar(fila, elem);
+                    printf("\nDeseja inserir outro comando? 1 p/ sim, 0 p/ nao: ");
+                    scanf("%d", &ctrl);
+                }while(ctrl!=0);
+                printf("Fila: ");
+
+                //Mover P
+                printf("\n|------------------------------|\n\n");
+                executar_comando1(fila, fase, tab, plr);
+                imprime_tab(tab);
+                imprime_player(plr);
+                if(tab[7][7] == 'P'){
+                    printf("\nParabens!!! Voce concluiu a %d fase\n\n",fase);
+                    fase++;
+                    tab[7][7] = 'O';
+                    tab[0][0] = 'P';
+                }else{
+                    tente++;
+                    if(tente < 3){
+                        printf("\nNão foi dessa vez, tente novamente!\nTentativa %d/3\n\n", tente);
+                    }else{
+                        printf("\nNão foi dessa vez, Fim de jogo...\n\n");
+                        exit(1);
+                    }
+                }
+                break;
+
+            case 2:
+                do{
+                    printf("\nDigite o comando: ");
+                    scanf("%d", &elem.cmd);
+                    printf("Digite o numero de vezes que este comando serao executado: ");
+                    scanf("%d", &elem.vez);
+                    empilhar(pilha, elem);
+                    printf("\nDeseja inserir outro comando? 1 p/ sim, 0 p/ nao: ");
+                    scanf("%d", &ctrl);
+                }while(ctrl!=0);
+
+                //Mover P
+                printf("\n|------------------------------|\n\n");
+                executar_comando2(pilha, fase, tab, plr);
+                imprime_tab(tab);
+                imprime_player(plr);
+                if(tab[7][7] == 'P'){
+                    printf("\nParabens!!! Voce concluiu a %d fase\n\n",fase);
+                    fase++;
+                    tab[7][7] = 'O';
+                    tab[0][0] = 'P';
+                }else{
+                    tente++;
+                    if(tente < 3){
+                        printf("\nNão foi dessa vez, tente novamente!\nTentativa %d/3\n\n", tente);
+                    }else{
+                        printf("\nNão foi dessa vez, Fim de jogo...\n\n");
+                        exit(1);
+                    }
+                }
+                break;
+
+            default:
+                break;
             }
-            sleep(10);
-        }while(tab[0][0]!='P');
-    }while(fase != 1); //fase != 3
+
+            sleep(2);
+    }while(fase != 3); //fase != 3
 
     return 0;
 }
